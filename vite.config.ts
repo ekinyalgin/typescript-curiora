776bcba1b667c88b4b5127705e8df123a0ce1613
ekinyalgin <ekinyalgin@gmail.com>
2024-10-08 15:04:19 +0300

    startup

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url' 
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
